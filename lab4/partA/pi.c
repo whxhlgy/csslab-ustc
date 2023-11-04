3898ff57f136a428756687786b4ace8330df212d
zjj <amazjj@email.ustc.edu.cn>
2023-11-04 07:35:57 +0000

    finish exercise 1 and 2

--- lab4/partA/pi.c
+++ lab4/partA/pi.c
@@ -2,15 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
-#define TODO()\
-do{\
-    extern int printf(char *, ...);\
-    printf("Add your code here: file %s, line %d\n", __FILE__, __LINE__);\
-}while(0)
-
-
-
 struct arg_t {
     float x;
     float y;
@@ -19,8 +10,13 @@ struct arg_t {
 void *start(void *arg) {
     // Exercise 2: Implement your code here
     // Add your code here:
-    TODO();
-
+    float x = ((struct arg_t*) arg)->x;
+    float y = ((struct arg_t*) arg)->y;
+    if (x*x + y*y < 1) {
+        pthread_exit((void *)1);
+    } else {
+        pthread_exit((void *)0);
+    }
 }
 
 int main(int argc, char **argv) {
--- lab4/partA/square.c
+++ lab4/partA/square.c
@@ -14,10 +14,22 @@ int main() {
     // Exercise 1: Implement your code here
     // Add your code here:
     for (long i = 0; i < N + 1; ++i) {
-        pthread_create(&tids[i], NULL, start, (void *)i);
+        if (pthread_create(&tids[i], NULL, start, (void *)i) != 0) {
+            perror("failed to creat thread!");
+            return 1;
+        }
     }
 
-    printf("sum: %ld\n", sum);  
+    for (long i = 0; i < N + 1; ++i) {
+        void *retval;
+        if (pthread_join(tids[i], &retval) != 0) {
+            perror("failed to join a thread!");
+            return 1;
+        }
+        sum += (long)retval;
+    }
+
+    printf("sum: %ld\n", sum);
     // 添加断言
     long expected_sum = 0;
     for (long i = 0; i <= N; ++ i) {
